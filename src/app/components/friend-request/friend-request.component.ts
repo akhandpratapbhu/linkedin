e79808cf63fadd72e60d42664f459f0869d1d64d
Akhand Pratap <akhand.pratap@netsmartz.local>
2024-07-18 19:03:28 +0530

    add img in friend request

--- src/app/components/friend-request/friend-request.component.ts
+++ src/app/components/friend-request/friend-request.component.ts
@@ -5,6 +5,7 @@ import { ConnectionProfileService } from '../../services/connection-profile.serv
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-friend-request',
@@ -15,33 +16,72 @@ import { MatIconModule } from '@angular/material/icon';
 })
 export class FriendRequestComponent {
 
-  friendRequest: any
-  constructor( private toastr: ToastrService, private connectionProfileService:ConnectionProfileService ,
-    public dialogRef: MatDialogRef<FriendRequestComponent>,
+  friendRequest: any=[]
+  imagePath!: string
+  constructor(private toastr: ToastrService, private connectionProfileService: ConnectionProfileService,
+    public dialogRef: MatDialogRef<FriendRequestComponent>, private userService: UserService,
     @Inject(MAT_DIALOG_DATA) public data: any
-  )  {
-    // Assuming data is an array and you want to access the first element
-if (Array.isArray(data)) {
-  this.friendRequest = data;
-}
+  ) {
+    if (Array.isArray(data)) {
+      this.friendRequest = data;
+      //console.log(this.friendRequest[0]?.creator.image);
 
+    }
+
+
+    this.friendRequest.forEach((creator: { image: string }) => {
+      if (creator.image) {
+        creator.image = this.getImageUrl(creator.image)
+      }
+      else {
+        creator.image = 'http://localhost:3000/api/feed/image/' + 'user.png'
+      }
+    // if (this.friendRequest?.creator?.image && this.friendRequest?.creator?.image.startsWith('https')) {
+    //   this.imagePath = this.imagePath
+    // }
+    // else {
+    //   this.loadprofileImage(this.imagePath)
+    // }
   }
+    )}
 
   closePopup() {
     this.dialogRef.close();
   }
+  getImageUrl(userImg: any): string {
 
-respondToFriendRequest(id: number, statusResponse: string) {
-  const status = {
-    status: statusResponse
-  };
+    if (userImg && userImg.startsWith('http')) {
+      return userImg;
+    }
 
-  this.connectionProfileService.responseToFriendRequest(id, status).subscribe(res => {
-    // Remove the responded friend request from the array
-    this.friendRequest = this.friendRequest.filter((request: { id: number; }) => request.id !== id);
-    // Optionally, show a toast notification
-    this.toastr.success(`Friend request ${statusResponse}`);
-    this.dialogRef.close(this.friendRequest);
-  });
-}
+    return 'http://localhost:3000/api/feed/image/' + (userImg || 'user.png');
+  }
+
+
+
+
+  loadprofileImage(imagePath: string) {
+
+    if (imagePath) {
+      this.imagePath = this.userService.getfullImagePath(imagePath)
+
+    }
+    else {
+      this.imagePath = this.userService.getDefaultfullImagePath()
+
+    }
+  }
+  respondToFriendRequest(id: number, statusResponse: string) {
+    const status = {
+      status: statusResponse
+    };
+
+    this.connectionProfileService.responseToFriendRequest(id, status).subscribe(res => {
+      // Remove the responded friend request from the array
+      this.friendRequest = this.friendRequest.filter((request: { id: number; }) => request.id !== id);
+      // Optionally, show a toast notification
+      this.toastr.success(`Friend request ${statusResponse}`);
+      this.dialogRef.close(this.friendRequest);
+    });
+  }
 }
